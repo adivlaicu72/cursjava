000809f37e85c2be6264ee50cd61d0ee08a7764f
adivlaicu72 <55662044+adivlaicu72@users.noreply.github.com>
2023-05-30 16:37:24 +0300

    curs7 intermediar

---
+++ src/curs7/TestShape.java
@@ -0,0 +1,15 @@
+package curs7;
+
+public class TestShape {
+
+	public static void main(String[] args) {
+		
+		Patrat patrat = new Patrat("Cerc", "Galben");
+		System.out.println(patrat.nume);
+		System.out.println(patrat.culoare);
+		
+		patrat.printDetails();
+
+	}
+
+}
